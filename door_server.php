1d62c1726ceb910d6b7e191cbd5abb0f479b80db
Benjamin Pontes <45019346+Benjaminxciv@users.noreply.github.com>
2019-09-26 16:54:10 -0500

    Door server progress

--- door_server.php
+++ door_server.php
@@ -1,35 +1,75 @@
-<html>
-  <body>
+
 <?php 
 
-  $open_times = array("8:30", "9:00", "12:00","13:00","18:00","19:00");
-  $array_length = count($cars);
-  // $bf_start     = new DateTime("8:30");
-  // $bf_end       = new DateTime("9:00");
-  // $lunch_start  = new DateTime("12:00");
-  // $lunch_end    = new DateTime("13:00");
-  // $dinner_start = new DateTime("18:00");
-  // $dinner_end   = new DateTime("19:00");
+$state         = $_REQUEST["state"];
+$cell_num      = $_REQUEST["cell_num"];
+$time          = strtotime($_REQUEST['time']);
   
-
-  $time = strtotime($_GET['time']);
-  if($time){
+if($time)
+{
   $curr_time = date("H:i", $time);
-  }else{
-    echo "invalid date";
+}else echo "invalid date";
+
+$alarm_message = "Warning! The alarm on cell $cell_num is active.";
+$alarm_state = "off";
+
+//Handle case of closed and unlocked for more than 5 minutes - replace with function if time
+if($state === "closed")
+{
+  $myfile = fopen("doorstate.txt", "r") or die("Unable to open file!");
+  
+  $i_cell_num = ((int)$cell_num - 1);
+  $linestop = (($i_cell_num * 3)+1);
+  
+  for($x = 0; $x < $linestop; $x++)
+  {
+    fgets($myfile);
   }
-
-//   for($x = 0; $x < $array_length; $x++) {
-//    // $temp_time1 = new DateTime($open_times[$x]);
-//     echo $open_times[$x];
-// }
-
   
-  $state = $_REQUEST["state"];
+  $prev_state = chop(fgets($myfile));
+  //echo "Current: $state <br>";
   
-  echo $curr_time;
-  print "<p>The door is: $state</p> ";
+  if($prev_state == "closed")
+    {
+      //echo "Previous: $prev_state <br>";
+      $prev_time = fgets($myfile);
+      
+      $t = strtotime($prev_time);
+  
+      $diff = ($time - $t)/60;
+      //echo "Time diff in minutes: $diff";
+      if( $diff > 5 | $diff < 0 )
+      {
+      //echo "<br> DOOR HAS BEEN UNLOCKED TO LONG";
+        $alarm_state = "on";
+      }
+    }
+}
+
+//Start of handling times doors are allowed to be open ..Only needed if state is open - replace with function if time
+if($state == "open");
+{
+$open_times = array("8:30", "9:00", "12:00","13:00","18:00","19:00");
+$array_length = count($open_times);
+
+if($state == "open")
+  {
+    for($x = 0; $x < $array_length; $x+=2)
+    {  
+        $t1 = strtotime($open_times[$x]);
+        $start_time = date("H:i", $t1);
+        $t2 = strtotime($open_times[$x+1]);
+        $end_time = date("H:i", $t2);
+      
+        if($curr_time >= $start_time && $curr_time <= $end_time){
+        echo $start_time;
+        echo " - $end_time";
+        echo "<br>";
+        break;
+        }else $alarm_state = "on";
+    }
+  }
+}
+  echo $alarm_state;
+ //print "<br> The door is: $state <br>";
 ?>
-
-</body>
-</html>
